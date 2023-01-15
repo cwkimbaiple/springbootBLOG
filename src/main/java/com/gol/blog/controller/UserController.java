edd2a9da8b3d0ed46b168fd422ca5ab968642a35
cwkimbaiple <cw.kim@baiple.com>
2023-01-15 17:38:31 +0900

    64강 완료

--- src/main/java/com/gol/blog/controller/UserController.java
+++ src/main/java/com/gol/blog/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.gol.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 //인증이 안된 사용자들이 출입할수 있는 경로는 /auth/* 허용
 // 그냥 주소가 / 이면 index.jsp허용, static 이하에 있는 /js/*, /css/*, /image/* 허용
@@ -11,14 +19,36 @@ public class UserController {
 
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
-		return"user/joinForm";
+		return "user/joinForm";
 	}
+
 	@GetMapping("/auth/loginForm")
 	public String loginForm() {
-		return"user/loginForm";
+		return "user/loginForm";
 	}
+
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakoCallback(String code) {
+
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "f4c1e20ba097c5b3d0ab62d483765d2d");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST,
+				kakaoTokenRequest, String.class);
+
+		return "카카오 토큰요청에 대한 응답" + response;
+	}
+
 	@GetMapping("/user/updateForm")
 	public String updateForm() {
-		return"user/updateForm";
+		return "user/updateForm";
 	}
 }
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -2,21 +2,23 @@
 	pageEncoding="UTF-8"%>
 <%@ include file="../layout/header.jsp"%>
 <div class="container">
-		<form 
-			action="/auth/loginProc" method="post">
-			<div class="form-group">
-				<label for="username">Username</label> <input type="text"
-					name="username" class="form-control" id="username"
-					placeholder="Enter Username">
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label> <input type="password"
-					name="password" class="form-control" id="password"
-					placeholder="Enter Password">
-			</div>
-			<br />
-			<button class="btn btn-primary text-center">로그인</button>
-		</form>
+	<form action="/auth/loginProc" method="post">
+		<div class="form-group">
+			<label for="username">Username</label> <input type="text"
+				name="username" class="form-control" id="username"
+				placeholder="Enter Username">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label> <input type="password"
+				name="password" class="form-control" id="password"
+				placeholder="Enter Password">
+		</div>
+		<br />
+		<button class="btn btn-primary text-center">로그인</button>
+		<a
+			href="https://kauth.kakao.com/oauth/authorize?client_id=f4c1e20ba097c5b3d0ab62d483765d2d&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img
+			height="38px" src="/image/kakao_login_medium_narrow.png" /></a>
+	</form>
 </div>
 
 <script src="/js/user.js"></script>
